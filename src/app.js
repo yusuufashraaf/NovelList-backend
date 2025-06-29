5079387ff63e95092c0d72af1a95fa13170e3136
Eyad Osama <eyadosama0616@gmail.com>
2025-06-29 23:24:11 +0300

    adding crud operation on authentication

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
+
+// Database connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use("/api/users", require("./routes/user.routes"));
+app.use("/api/v1/users", require("../routes/userRoute"));
 
 // Error handler
 app.use((err, req, res, next) => {
