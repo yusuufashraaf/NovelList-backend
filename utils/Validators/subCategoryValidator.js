5d97e0ea9d767dee82fce6273f788506796932c0
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-06-30 01:29:56 +0300

    feat: add validation middleware for MongoDB ID and implement validators for Brand, Category, Product, and SubCategory

---
+++ utils/Validators/subCategoryValidator.js
@@ -0,0 +1,36 @@
+const  { check } =require( "express-validator");
+const  validateMongoId = require( "../../middlewares/validatorMiddleware");
+
+
+
+const addSubCategoryValidator = [
+    check("name").notEmpty().withMessage("Sub Category name is required")
+        .isLength({min:3}).withMessage("Sub Category name must be at least 3 characters")
+        .isLength({max:20}).withMessage("Sub Category name must be less than 20 characters"),
+    check("category").isMongoId().withMessage("Invalid Category Id format"),
+    validateMongoId
+]
+
+const getSubCategoryValidator = [
+    check("id").isMongoId().withMessage("Invalid Sub Category Id format"),
+    validateMongoId
+]
+
+const updateSubCategoryValidator = [
+    check("id").isMongoId().withMessage("Invalid Sub Category Id format"),
+    check("name").notEmpty().withMessage("Sub Category name is required"),
+
+    validateMongoId
+]
+
+const deleteSubCategoryValidator = [
+    check("id").isMongoId().withMessage("Invalid Sub Category Id format"),
+    validateMongoId
+]
+
+module.exports = {
+    addSubCategoryValidator,
+    getSubCategoryValidator,
+    updateSubCategoryValidator,
+    deleteSubCategoryValidator
+}
