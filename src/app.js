eba873cbe55e2f16820841262df71875721d98e7
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-06-29 20:58:24 +0300

    handling paypal integration still needed connection with database mongo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/api/users", require("./routes/user.routes"));
+// app.use("/api/users", require("./routes/user.routes"));
+
+//paypal
+const paypalRoutes = require ("./../routes/paypal.route");
+app.use("/buy", paypalRoutes);
+
+
 
 // Error handler
 app.use((err, req, res, next) => {
