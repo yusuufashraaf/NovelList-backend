eda25cab73526d9302097b36faa4135192269aa7
Eyad Osama <eyadosama0616@gmail.com>
2025-06-30 16:48:27 +0300

    handling registering validations

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,26 @@
-const {getAllUsers, getUserById, createUser, updateUser, deleteUser} = require("../controllers/userController");
+const express = require("express");
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
 
-const router = require("express").Router();
+const { createUserValidator } = require("../middlewares/userValidator");
+const validateMongoId = require("../middlewares/validatorMiddleware");
+
+const router = express.Router();
 
 router
-    .route("/")
-    .get(getAllUsers)
-    .post(createUser);
+  .route("/")
+  .get(getAllUsers)
+  .post(createUserValidator, validateMongoId, createUser);
+
 router
-    .route("/:id")
-    .get(getUserById)
-    .patch(updateUser)
-    .delete(deleteUser);
+  .route("/:id")
+  .get(getUserById)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
