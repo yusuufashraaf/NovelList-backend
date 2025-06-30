eb7b860a84712ee7112a1903204e7a35cfeaded9
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-07-01 01:57:10 +0300

    Refactor userRoute.js: remove merge conflict markers and clean up import statements

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-const {Router} = require("express");
-const {getAllUsers, getUserById, createUser, updateUser, deleteUser} = require("../controllers/userController");
- 
-const router = Router();
-=======
+
 const express = require("express");
 const {
   getAllUsers,
@@ -17,7 +12,6 @@ const { createUserValidator } = require("../middlewares/userValidator");
 const validateMongoId = require("../middlewares/validatorMiddleware");
 
 const router = express.Router();
->>>>>>> 09272e8c422533690ca86ca99be8426237bcb5dd
 
 router
   .route("/")
