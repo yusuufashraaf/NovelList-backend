5b919be3ab7b8f6b7ac139197fe435c90e427845
Youssef Ashraf <yusuufashraaf@icloud.com>
2025-07-01 05:24:02 +0300

    feat(api): support filtering products by genre, author, rating, and price range via query parameters
    fix(api): correctly parse price range filters from query string (price[gte], price[lte])
    
    fix(api): return empty product array instead of 404 when no products match filters

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-const paypalRoutes = require ("../routes/paypalRoute");
-
+const cors = require("cors");
+const paypalRoutes = require("../routes/paypalRoute");
 
 const errorHandel = require("../middlewares/errorHandel");
 const AppError = require("../utils/AppError");
@@ -12,36 +11,34 @@ const subCategoryRouter = require("../routes/subCategory");
 const brandRouter = require("../routes/brand");
 const productRouter = require("../routes/product");
 const userRouter = require("../routes/userRoute");
+const qs = require("qs");
 
 const app = express();
 app.use(cors());
 
 app.use(morgan("dev"));
 
+app.set("query parser", (str) => qs.parse(str));
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use("/api/v1/users",userRouter);
-
-app.use("/api/v1/categories",categoryRouter);
-app.use("/api/v1/subCategories",subCategoryRouter);
-app.use("/api/v1/brands",brandRouter);
-app.use("/api/v1/products",productRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/subCategories", subCategoryRouter);
+app.use("/api/v1/brands", brandRouter);
+app.use("/api/v1/products", productRouter);
 
 //paypal
 app.use("/buy", paypalRoutes);
 
 // 404 handler
-app.use((req,res,next)=>{
-    next(new AppError(404,`can't find this route ${req.originalUrl}`));
-})
+app.use((req, res, next) => {
+  next(new AppError(404, `can't find this route ${req.originalUrl}`));
+});
 
 // Global error handler
-app.use(errorHandel)
-
-
-
+app.use(errorHandel);
 
 module.exports = app;
