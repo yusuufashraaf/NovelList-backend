69eaaf34214971518eb1b429af8abb3629f4e6cf
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-07-06 15:05:05 +0300

    refactor: enhance authentication middleware with detailed logging and improved token validation

--- middlewares/Authenticate.js
+++ middlewares/Authenticate.js
@@ -1,25 +1,34 @@
-const User = require('./../models/userAuthModel');
+const User = require("./../models/userAuthModel");
 
- const Authenticate =async (req,res,next)=> {
-        
-        const token = req.headers.authorization;
-        
-        let checkToken=token;
-            if (token.startsWith('Bearer ')) { // because browser send it by default
-                checkToken =token.split(' ')[1];
-            }
-    
-        const user = await User.verifyUser(checkToken)
-        if(user){
-            req.user = user; 
-            next();          
-        }else{
-            const error = new Error("You are not Authorized");
-            error.status = 401; 
-            return next(error);
-            
-        }
+const Authenticate = async (req, res, next) => {
+  const token = req.headers.authorization;
+  console.log("token:", token);
 
-}
+  console.log("== Incoming request ==");
+  console.log("Authorization:", token);
+  console.log("Headers:", req.headers);
+  console.log("== End of request ==");
+
+  // protect routes from empty token requests
+  if (!token || !token.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Please login first", status: 401 });
+  }
+
+  let checkToken = token.split(" ")[1];
+
+  console.log("checkToken-test:", checkToken);
+
+  const user = await User.verifyUser(checkToken);
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: "You are not Authorized", status: 401 });
+  }
+
+  req.user = user;
+  console.log("test-User verified:", user);
+  next();
+};
 
 module.exports = Authenticate;
