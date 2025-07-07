3f13cf9c71c8d6b4c6298a18de53c4cf36fa8724
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-07-07 13:47:15 +0300

    testing

--- middlewares/Authenticate.js
+++ middlewares/Authenticate.js
@@ -2,6 +2,8 @@ const User = require("./../models/userAuthModel");
 
 const Authenticate = async (req, res, next) => {
   const token = req.headers.authorization;
+  console.log(token);
+  
   // console.log("token:", token);
 
   // console.log("== Incoming request ==");
--- routes/paypalRoute.js
+++ routes/paypalRoute.js
@@ -6,7 +6,8 @@ const paypalController = require('../controllers/paypalController');
 
 
 const validateOrder = require('../middlewares/validateOrder');
+const Authenticate = require('../middlewares/Authenticate');
 // router.get('',paypalController);
-router.post('/create-test-order',validateOrder.validateCreateOrder,validateOrder.handleValidationErrors,paypalController.createOrder)
+router.post('/create-test-order',Authenticate,validateOrder.validateCreateOrder,validateOrder.handleValidationErrors,paypalController.createOrder)
 router.get('/confirm',paypalController.captureOrder)
 module.exports=router;
