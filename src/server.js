0a65febb82bfc41b32ed398833a17dc25800ddef
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-07-21 02:24:37 +0300

    adding server code

--- src/server.js
+++ src/server.js
@@ -5,19 +5,19 @@ const server = http.createServer(app);
 const connectDB = require("../config/connectDB");
 require("dotenv").config();
 
-const io = require('socket.io')(server, {
+const io = require("socket.io")(server, {
   cors: {
-    origin: 'http://localhost:4200',
-    credentials: true
-  }
+    origin: ["http://localhost:4200", "https://novel-nest-two.vercel.app"],
+    credentials: true,
+  },
 });
-app.set('io', io);
+
+app.set("io", io);
 
 const adminSocketMap = new Map();
 
 io.on("connection", async (socket) => {
-
-  socket.on("connectToserver", async (token,data) => {
+  socket.on("connectToserver", async (token, data) => {
     const user = await User.verifyUser(token);
     if (!user) {
       socket.emit("unauthorized");
@@ -25,35 +25,31 @@ io.on("connection", async (socket) => {
     }
 
     //  Admin logic
-    if (user.role === 'admin') {
+    if (user.role === "admin") {
       const userId = user._id.toString();
       adminSocketMap.set(userId, socket.id);
-      socket.userId = userId; 
+      socket.userId = userId;
       console.log("added the socket id to admin");
     }
     //  User logic
-    if (user.role === 'user') {
-      if(adminSocketMap.size >0){
-        for(const [key, value] of adminSocketMap){
-          if(data){
-            io.to(value).emit("newNotification",data);
+    if (user.role === "user") {
+      if (adminSocketMap.size > 0) {
+        for (const [key, value] of adminSocketMap) {
+          if (data) {
+            io.to(value).emit("newNotification", data);
           }
         }
       }
     }
   });
 
-  
   socket.on("disconnect", () => {
     if (socket.userId && adminSocketMap.has(socket.userId)) {
       adminSocketMap.delete(socket.userId);
       console.log(`Admin with ID ${socket.userId} disconnected and removed from map.`);
     }
   });
-
- 
 });
-
 
 const PORT = process.env.PORT || 3000;
 
@@ -61,9 +57,6 @@ const Server = server.listen(PORT, () => {
   connectDB();
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
 
 //  for any error out of server or express like  lose connection to database or different error in promise functions
 process.on("unhandledRejection", (err) => {
